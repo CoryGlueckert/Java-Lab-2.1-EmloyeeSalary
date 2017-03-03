8ce97d4c83c564b608f0049cae8962453214be98
Cory Glueckert <Cory Glueckert>
2017-03-02 18:00:56 -0700

    Driver system.out.printf has been changed to print name and yearly salary with a 10% raise

--- src/Driver.java
+++ src/Driver.java
@@ -4,10 +4,10 @@ public class Driver {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		Employee employee1 = new Employee ("Cory", "Glueckert", 3000.00 );
-		Employee employee2 = new Employee ("Jack", "Frost", 5000.00 );
-		System.out.printf("employee1 name is: %s");
-		System.out.printf("employee2 name is: %s");
+		Employee employee1 = new Employee ("Cory", "Glueckert", 3000d );
+		Employee employee2 = new Employee ("Jack", "Frost", 5000d );
+		System.out.printf("employee1 name is: %s %s $%.2f \n", employee1.getFirstName(),employee1.getLastName(), employee1.yearlySalary() + employee1.raise());
+		System.out.printf("employee2 name is: %s %s $%.2f", employee2.getFirstName(),employee2.getLastName(), employee2.yearlySalary() + employee2.raise());
 	}
 
 }
